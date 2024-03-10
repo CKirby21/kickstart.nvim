be4fef38787097ac873e780a508b1705239f20a4
Caden Kirby <cadenkirby@outlook.com>
2024-03-10 08:03:05 -0400

    Update

--- init.lua
+++ init.lua
@@ -491,10 +491,11 @@ require('lazy').setup({
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
-                -- gopls = {},
-                -- pyright = {},
-                -- rust_analyzer = {},
+                clangd = {},
+                gopls = {},
+                pyright = {},
+                rust_analyzer = {},
+                bashls = {},
                 -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
                 --
                 -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -743,7 +744,7 @@ require('lazy').setup({
 
             ---@diagnostic disable-next-line: missing-fields
             require('nvim-treesitter.configs').setup {
-                ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+                ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'rust', 'cpp', 'python' },
                 -- Autoinstall languages that are not installed
                 auto_install = true,
                 highlight = { enable = true },
