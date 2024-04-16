5c087604bd62ccbb71222f9afb6a994e3a976fed
ckirby <ckirby@4dtechsolutions.com>
2024-04-16 09:09:01 -0400

    Update

--- init.lua
+++ init.lua
@@ -346,10 +346,10 @@ require('lazy').setup({
             local builtin = require 'telescope.builtin'
             vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
             vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
             vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
             vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sf', builtin.live_grep, { desc = '[S]earch [F]iles' })
             vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -374,9 +374,18 @@ require('lazy').setup({
             end, { desc = '[S]earch [/] in Open Files' })
 
             -- Shortcut for searching your neovim configuration files
-            vim.keymap.set('n', '<leader>sn', function()
+            vim.keymap.set('n', '<leader>fn', function()
                 builtin.find_files { cwd = vim.fn.stdpath 'config' }
-            end, { desc = '[S]earch [N]eovim files' })
+            end, { desc = '[F]ind [N]eovim files' })
+
+            local brain = os.getenv('HOME') .. '/repos/brain'
+            vim.keymap.set('n', '<leader>fb', function()
+                builtin.find_files { cwd = brain }
+            end, { desc = '[F]ind [B]rain files' })
+
+            vim.keymap.set('n', '<leader>sb', function()
+                builtin.live_grep { cwd = brain }
+            end, { desc = '[S]earch [B]rain' })
         end,
     },
 
